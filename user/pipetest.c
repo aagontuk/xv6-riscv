75a796f6c460360bdf1572c9e25e61d13599c5d0
Ashfaqur Rahaman <ashfaqur.rahaman@utah.edu>
2021-08-27 19:47:57 -0600

    Added check for data in the receive side

--- user/pipetest.c
+++ user/pipetest.c
@@ -1,4 +1,8 @@
-/* Assignment: 1 */
+/* 
+ * Assignment: 1 
+ * Md Ashfaqur Rahaman
+ * 
+ */
 
 #include "kernel/types.h"
 #include "user/user.h"
@@ -6,12 +10,48 @@
 #define BUFSIZE 128
 #define DATASIZE 10485760
 
+// from FreeBSD.
+int
+do_rand(unsigned long *ctx)
+{
+/*
+ * Compute x = (7^5 * x) mod (2^31 - 1)
+ * without overflowing 31 bits:
+ *      (2^31 - 1) = 127773 * (7^5) + 2836
+ * From "Random number generators: good ones are hard to find",
+ * Park and Miller, Communications of the ACM, vol. 31, no. 10,
+ * October 1988, p. 1195.
+ */
+    long hi, lo, x;
+
+    /* Transform to [1, 0x7ffffffe] range. */
+    x = (*ctx % 0x7ffffffe) + 1;
+    hi = x / 127773;
+    lo = x % 127773;
+    x = 16807 * lo - 2836 * hi;
+    if (x < 0)
+        x += 0x7fffffff;
+    /* Transform to [0, 0x7ffffffd] range. */
+    x--;
+    *ctx = x;
+    return (x);
+}
+
+unsigned long rand_next = 1;
+
+int
+rand(void)
+{
+    return (do_rand(&rand_next));
+}
+
 int main(void) {
     int p[2];
     int pid;
     int start, end;
     int nsend = DATASIZE/(BUFSIZE*4);
-    int nbytes, tbytes = 0;
+    int rbytes, tbytes = 0;
+    int wbytes;
     int writebuffer[BUFSIZE];
     int readbuffer[BUFSIZE];
 
@@ -24,30 +64,58 @@ int main(void) {
 
     if (pid == -1) {
         fprintf(2, "fork() failed!\n"); 
-        exit(-2);
+        exit(-1);
     }
     else if (pid == 0) {
         close(p[0]);
+        
         for (int n = 0; n < nsend; n++) {
             for (int i = 0; i < BUFSIZE; i++) {
-                writebuffer[i] = i;
+                writebuffer[i] = rand();
             }
-            write(p[1], (void *)writebuffer, sizeof(int)*BUFSIZE);
+            
+            wbytes = write(p[1], (void *)writebuffer, sizeof(int)*BUFSIZE);
+            if (wbytes == -1) {
+                fprintf(2, "Error writing to pipe!\n"); 
+                exit(-1);
+            }
+            else if (wbytes < sizeof(int)*BUFSIZE) {
+                fprintf(2, "Error writing to pipe!\n"); 
+                exit(-1);
+            }
         }
+        
         close(p[1]);
     }
     else {
         close(p[1]);
-	start = uptime();
+	    start = uptime();
+        
         for (int n = 0; n < nsend; n++) {
-            nbytes = read(p[0], (void *)readbuffer, sizeof(int)*BUFSIZE);
-            tbytes += nbytes;
+            rbytes = read(p[0], (void *)readbuffer, sizeof(int)*BUFSIZE);
+            
+            if (rbytes == -1) {
+                fprintf(2, "Error reading from pipe!\n");
+                exit(-1);
+            }
+            
+            tbytes += rbytes;
+
+            /* Check integers */
+            for (int i = 0; i < BUFSIZE; i++) {
+                if (readbuffer[i] != rand()) {
+                    fprintf(2, "Data miss match!\n"); 
+                    exit(-3);
+                } 
+            }
         }
-	end = uptime();
+	    
+        end = uptime();
         close(p[0]);
         wait((int *)0);
+        
         fprintf(1, "Received: %d bytes in ", tbytes);
-	fprintf(1, "%d ticks.\n", end - start);
+	    fprintf(1, "%d ticks.\n", end - start);
     }
     
     exit(0);
